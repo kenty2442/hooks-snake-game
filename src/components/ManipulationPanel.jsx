acd97738f1d41d4c0d58c74733ed1aa58e663471
kenty2442 <mcky874@gmail.com>
2021-09-03 15:30:07 +0900

    first-commit

---
+++ src/components/ManipulationPanel.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ManipulationPanel = () => {
+    return (
+        <div className="manipuration-panel">
+            <button>←</button>
+            <button>↑</button>
+            <button>→</button>
+            <button>↓</button>
+        </div>
+    );
+};
+
+export default ManipulationPanel;
