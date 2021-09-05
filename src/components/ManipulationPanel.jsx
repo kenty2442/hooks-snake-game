8d2e0b4fd6ac586b96816ddbd6aa9fe2d57561b0
kenty2442 <mcky874@gmail.com>
2021-09-05 15:49:07 +0900

    Complete

--- src/components/ManipulationPanel.jsx
+++ src/components/ManipulationPanel.jsx
@@ -1,4 +1,11 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+    faArrowLeft,
+    faArrowUp,
+    faArrowDown,
+    faArrowRight
+} from '@fortawesome/free-solid-svg-icons';
 
 const ManipulationPanel = ({ onChange }) => {
     const onUp = () => onChange('up');
@@ -7,10 +14,20 @@ const ManipulationPanel = ({ onChange }) => {
     const onDown = () => onChange('down');
     return (
         <div className="manipuration-panel">
-            <button onClick={onLeft}>←</button>
-            <button onClick={onUp}>↑</button>
-            <button onClick={onRight}>→</button>
-            <button onClick={onDown}>↓</button>
+            <button className='manipulation-btn btn btn-move btn-left' onClick={onLeft}>
+                <FontAwesomeIcon icon={faArrowLeft} />
+            </button>
+            <div>
+                <button className='manipulation-btn btn btn-move btn-up' onClick={onUp}>
+                    <FontAwesomeIcon icon={faArrowUp} />
+                </button>
+                <button className='manipulation-btn btn btn-move btn-down' onClick={onDown}>
+                    <FontAwesomeIcon icon={faArrowDown} />
+                </button>
+            </div>
+            <button className='manipulation-btn btn btn-move btn-right' onClick={onRight}>
+                <FontAwesomeIcon icon={faArrowRight} />
+            </button>
         </div>
     );
 };
